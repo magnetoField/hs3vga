1f643a87208907060603bc197244a8429815156a
magnetofield <34056090+magnetoField@users.noreply.github.com>
2026-02-28 20:20:20 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_vga_example(
+module tt_um_magnetofield_hs3text(
   input  wire [7:0] ui_in,    // Dedicated inputs
   input  wire [7:0] uio_in,   // IOs: Input path
   output wire [7:0] uo_out,   // Dedicated outputs
